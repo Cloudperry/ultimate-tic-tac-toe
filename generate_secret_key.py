cb3a90f36c74f731dd6ed82e6920571ede15a4a4
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-04 04:21:40 +0300

    Fix key generator behavior if the env file doesn't exist

--- generate_secret_key.py
+++ generate_secret_key.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 import secrets
+env_file_name = ".env"
 
 def generate_key_if_not_exist():
+    open(env_file_name, 'a').close()
     has_secret_key = False
-    with open(".env") as env_file:
+    with open(env_file_name) as env_file:
         for line in env_file:
             if line.startswith("SECRET_KEY"):
                 has_secret_key = True
 
-    with open(".env", "a") as env_file:
+    with open(env_file_name, "a") as env_file:
         if not has_secret_key:
             env_file.write(f"SECRET_KEY={secrets.token_hex(32)}")
