a28bbdfed254adb284414ca5e3f11f2ba9939331
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-23 18:08:10 +0300

    Change app.py and users.py to use init.py

--- users.py
+++ users.py
@@ -1,6 +1,6 @@
-from app import db
 from flask import session
 from werkzeug.security import check_password_hash, generate_password_hash
+from init import db
 
 def login(username, password):
     sql = "SELECT id, password FROM users WHERE username=:username"
