7303f02a005bf12b433a75c59f188f9dcd4b51db
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-24 18:45:12 +0300

    Add stats.py with a more descriptive name (it will be used only for game stats)

--- stats.py
+++
@@ -1,7 +0,0 @@
-from init import db
-
-#Here I will add code related to global stats like total games played and current active games
-#Code for personal stats like winrate and games history will also be added here 
-
-def game_count() -> int:
-    pass
