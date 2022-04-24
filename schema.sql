7b34def71210693f8d0fa1fdd41623f7e5bb50cf
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-25 02:29:22 +0300

    Add not null on every field that isn't a primary key, because there are no optional fields. Add unique constraint on pairs and columns where it makes sense.

--- schema.sql
+++ schema.sql
@@ -2,25 +2,29 @@ CREATE TYPE stats_visibility AS ENUM ('public', 'friends', 'private');
 
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
-    username TEXT UNIQUE,
-    password TEXT,
-	stats_visibility stats_visibility
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,
+	stats_visibility stats_visibility NOT NULL
 );
 
 -- This table will be used both to keep track of friend requests and as a friends list
-CREATE TABLE friend_requests (
-	sender_id INTEGER REFERENCES users,
-	recipient_id INTEGER REFERENCES users,
-	accepted BOOL
+CREATE TABLE friends (
+	sender_id INTEGER REFERENCES users NOT NULL,
+	recipient_id INTEGER REFERENCES users NOT NULL,
+	accepted BOOL NOT NULL,
+	UNIQUE (sender_id, recipient_id),
+	UNIQUE (recipient_id, sender_id)
 );
 
 -- Even inactive lobbies are stored in the database so that chat logs can be checked later
 CREATE TABLE lobbies (
 	id SERIAL PRIMARY KEY,
-	owner_id INTEGER REFERENCES users,
-	player2_id INTEGER REFERENCES users,
-	spectator_count INTEGER, 
-	active BOOL
+	owner_id INTEGER REFERENCES users NOT NULL,
+	player2_id INTEGER REFERENCES users NOT NULL,
+	spectator_count INTEGER NOT NULL,
+	active BOOL NOT NULL,
+	UNIQUE (owner_id, player2_id),
+	UNIQUE (player2_id, owner_id)
 	-- Last updated date could be added to make automatically removing lobbies that users forgot to close
 );
 
@@ -32,18 +36,18 @@ CREATE TABLE lobbies (
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
-	lobby_id INTEGER REFERENCES lobbies,
-    user_id INTEGER REFERENCES users,
-    content TEXT,
-    sent_at TIMESTAMP
+	lobby_id INTEGER REFERENCES lobbies NOT NULL,
+    user_id INTEGER REFERENCES users NOT NULL,
+    content TEXT NOT NULL,
+    sent_at TIMESTAMP NOT NULL
 );
 
 CREATE TYPE player_number AS ENUM ('1', '2');
 
 CREATE TABLE game_stats (
-	winner_id INTEGER REFERENCES users,
-	loser_id INTEGER REFERENCES users,
-	move_count INTEGER,
-	played_on TIMESTAMP
+	winner_id INTEGER REFERENCES users NOT NULL,
+	loser_id INTEGER REFERENCES users NOT NULL,
+	move_count INTEGER NOT NULL,
+	played_on TIMESTAMP NOT NULL
 	-- Count of won subboards by each player could be added to this table
 );
