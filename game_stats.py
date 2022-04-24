4ff8b2b14a1ff4f5bdc92166617e1488f64025f9
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-24 19:09:09 +0300

    Add basic stats retrieval functions

--- game_stats.py
+++ game_stats.py
@@ -4,10 +4,10 @@ import users
 #Here I will add code related to stats about past games (from table game_stats)
 #Code for personal stats like winrate and games history will also be added here 
 
-def game_count() -> int:
+def games_played() -> int:
     return db.session.execute("SELECT count(*) FROM game_stats").fetchone()[0]
 
-def user_game_count() -> int: #This function hasn't been tested yet
+def user_games_played() -> int: #This function hasn't been tested yet
     sql = "SELECT count(*) FROM game_stats WHERE user_id=:id"
     return db.session.execute(sql, {"id":users.user_id()}).fetchone()[0]
 
