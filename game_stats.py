56d68fdc9ac20dcdf27a8ec314f9d6596931a3c2
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-24 23:12:41 +0300

    Add function for parsing game results from db format into a dict that is useful in personal stats. Return game history in that format.

--- game_stats.py
+++ game_stats.py
@@ -8,9 +8,23 @@ def games_played() -> int:
     return db.session.execute("SELECT count(*) FROM game_stats").fetchone()[0]
 
 def user_games_played() -> int: #This function hasn't been tested yet
-    sql = "SELECT count(*) FROM game_stats WHERE player1=:id OR player2=:id"
+    sql = "SELECT count(*) FROM game_stats WHERE winner_id=:id OR loser_id=:id"
     return db.session.execute(sql, {"id":users.user_id()}).fetchone()[0]
 
+def parse_game_for_user(game: dict) -> dict:
+    result = {}
+    if game["winner_id"] == users.user_id():
+        result["result"] = "win"
+        result["opponent"] = users.username_from_id(game["loser_id"])
+    else:
+        result["result"] = "loss"
+        result["opponent"] = users.username_from_id(game["winner_id"])
+    result["move_count"] = game["move_count"]
+    result["played_on"] = game["played_on"]
+    return result
+
 def user_n_last_games(n: int):
-    sql = "SELECT * FROM game_stats WHERE player1=:id OR player2=:id ORDER BY played_on"
-    return db.session.execute(sql, {"id":users.user_id()}).fetchall()
+    sql = "SELECT * FROM game_stats WHERE winner_id=:id OR loser_id=:id ORDER BY played_on"
+    games = db.session.execute(sql, {"id":users.user_id()}).all()
+    games = [qr._mapping for qr in games]
+    return list(map(parse_game_for_user, games))
