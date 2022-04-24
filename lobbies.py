d59227e4aa36550e46ccc093c35be2bf8debd2c9
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-24 19:15:23 +0300

    Add function for retrieving the number of active games

--- lobbies.py
+++ lobbies.py
@@ -1,3 +1,6 @@
 from init import db
 
 #Here I will add code related to managing game lobbies and messages
+#Retrieving stats about active games will also be added here
+def active_games() -> int:
+    return db.session.execute("SELECT count(*) FROM lobbies WHERE active").fetchone()[0]
