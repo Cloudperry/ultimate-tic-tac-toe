0c9df4fabc25891d02041bfff67c3ac447cd7dce
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-04-25 04:53:16 +0300

    Add hack to hopefully make heroku work

--- init.py
+++ init.py
@@ -7,6 +7,6 @@ app = Flask(__name__)
 
 generate_key_if_not_exist()
 app.secret_key = getenv("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
