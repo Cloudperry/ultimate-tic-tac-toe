d3adb6e0a2478282709e657f5a8c953e27036ff2
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-05-08 00:59:56 +0300

    Replace lobby active boolean column with lobby_status enum

--- schema.sql
+++ schema.sql
@@ -17,25 +17,19 @@ CREATE TABLE friends (
 	UNIQUE (recipient_id, sender_id)
 );
 
-CREATE TYPE lobby_status AS ENUM ('ingame', 'open', 'inactive'); 
+CREATE TYPE lobby_status AS ENUM ('ingame', 'ready', 'waiting', 'inactive'); 
 -- Even inactive lobbies are stored in the database so that chat logs can be checked later
 CREATE TABLE lobbies (
 	id INTEGER PRIMARY KEY,
 	owner_id INTEGER REFERENCES users NOT NULL,
 	player2_id INTEGER REFERENCES users,
-	active BOOL NOT NULL, --Replace this with lobby_status, so the app can redirect from the lobby page straight to the game when the lobby is in game
+	status lobby_status NOT NULL,
 	visibility visibility NOT NULL,
 	spectators_allowed BOOL NOT NULL,
 	UNIQUE (owner_id, player2_id),
 	UNIQUE (player2_id, owner_id)
 	-- Last updated date could be added for automatically removing lobbies that users forgot to close
 );
-
--- I will probably not store the game state in the database
--- I think that would be just bad design, because I will need 3 different representations for the game state already:
-	-- a server-side in memory representation using Python (for checking the game logic) 
-	-- an in memory representation on the client (for rendering the board)
-	-- a representation that can be sent from the client to the server (for updating the client with the other players moves)
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
