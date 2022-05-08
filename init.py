e907790e95ef016901fadc89f33c2018f0b152f8
Roni Hokkanen <roni.hokkanen@protonmail.com>
2022-05-08 17:59:16 +0300

    Add server-sent events using flask-sse. Add load-dotenv call so that .env gets loaded when using gunicorn locally. Server now needs Redis, because flask-sse uses it. Also the server won't work with flask run, because flask-sse requires gunicorn with gevent.

--- init.py
+++ init.py
@@ -1,13 +1,21 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_sse import sse
 from os import getenv, environ
+from dotenv import load_dotenv
+# Modules from this project
 from generate_secret_key import generate_key_if_not_exist
 
+# Initialize Flask app
 app = Flask(__name__)
-
+load_dotenv()
 if 'HEROKU' not in environ:
     generate_key_if_not_exist()
 app.secret_key = getenv("SECRET_KEY")
+# Initialize db
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL").replace("postgres://", "postgresql://", 1)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+# Initialize server-sent events
+app.config["REDIS_URL"] = "redis://localhost"
+app.register_blueprint(sse, url_prefix='/updates')
